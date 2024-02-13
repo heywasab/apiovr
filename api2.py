4b18af5225eab7acfddefddf82c0beddd6e86bcc
heywasab <155819922+heywasab@users.noreply.github.com>
2024-02-13 18:10:14 +0500

    Add files via upload

---
+++ api2.py
@@ -0,0 +1,28 @@
+import requests
+# 1
+# a={'id': '1', 'title': 'none'}
+# response=requests.put('https://jsonplaceholder.typicode.com/posts/1',params=a)
+# data=response.json()
+# print(response.status_code)
+
+
+
+#2
+
+
+# a={'title': 'none', 'body': 'none', 'userId': '1'}
+# response=requests.patch('https://jsonplaceholder.typicode.com/posts/1',params=a)
+# data=response.json()
+# print(response.status_code)
+
+
+#3
+
+
+# response=requests.delete('https://jsonplaceholder.typicode.com/posts/1',params=a)
+# data=response.json()
+# print(response.status_code)
+
+
+
+
